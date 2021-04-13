cdd9ce905f49f5c1321304b9c9e8e729ee7512b8
Ty Kentaro Okazaki <tokazaki@uw.edu>
2021-04-13 15:09:48 -0700

    add loops

--- inc03a_practice.R
+++ inc03a_practice.R
@@ -6,7 +6,7 @@ km2mi <- function(km) {
 
 KM2MI <- function(km) km/1.609344
 
-#independent solution - m & f to km calculations
+# independent solution - m & f to km calculations
 mf2km <- function(m, f) {
   x <- m*1.609344
   y <- f*0.0003048
@@ -14,12 +14,12 @@ mf2km <- function(m, f) {
   return(km)
 }
 
-#class solution - m & f to km calculations8
+# class solution - m & f to km calculations8
 MF2KM <- function(mi, ft) {
   mi*1.609344 + ft*0.3048/1000
 }
 
-#even or odd function test
+# even or odd function test
 n <- 6
 if(n %% 2 == 1) {
   cat("number", n, "is odd\n")
@@ -27,11 +27,39 @@ if(n %% 2 == 1) {
   cat("number", n, "is even\n")
 }
 
-#function evenOdd that tells if a number is even or odd
+# function evenOdd that tells if a number is even or odd
+# Remainder 1 after dividing by 2, odd number 
 evenOdd <- function(n) {
   if(n %% 2 == 1) {
-      cat("odd\n")
+    cat("number", n, "is odd\n")
   } else {
-      cat("even\n")
+    cat("number", n, "is even\n")
   }
+}
+
+# leaving out else
+Odd <- function(n) {
+  if(n %% 2 == 1) {
+    cat("odd\n")
+  }
+}
+
+## Loops
+for(i in 1:5) {
+  cat("hi there!\n")
+}
+
+## Loop counter
+for(i in 1:5) {
+  cat(i, "iteration\n")
+}
+
+## Even or odd?
+for(i in 1:10) {
+  evenOdd(i)
+}
+
+words <- c("Hello", "world", "here", "come", "I")
+for(word in words) {
+  cat(word, "\n")
 }
