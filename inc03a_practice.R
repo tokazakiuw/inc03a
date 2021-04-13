077a5cc633f7551e4c8ca340fe60db2144950667
Ty Kentaro Okazaki <tokazaki@uw.edu>
2021-04-13 14:57:22 -0700

    add control structures

--- inc03a_practice.R
+++ inc03a_practice.R
@@ -6,7 +6,7 @@ km2mi <- function(km) {
 
 KM2MI <- function(km) km/1.609344
 
-#independent solution
+#independent solution - m & f to km calculations
 mf2km <- function(m, f) {
   x <- m*1.609344
   y <- f*0.0003048
@@ -14,7 +14,24 @@ mf2km <- function(m, f) {
   return(km)
 }
 
-#class solution
+#class solution - m & f to km calculations8
 MF2KM <- function(mi, ft) {
   mi*1.609344 + ft*0.3048/1000
+}
+
+#even or odd function test
+n <- 6
+if(n %% 2 == 1) {
+  cat("number", n, "is odd\n")
+} else {
+  cat("number", n, "is even\n")
+}
+
+#function evenOdd that tells if a number is even or odd
+evenOdd <- function(n) {
+  if(n %% 2 == 1) {
+      cat("odd\n")
+  } else {
+      cat("even\n")
+  }
 }
